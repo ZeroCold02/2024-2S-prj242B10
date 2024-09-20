7fe04f9b6f1970a1684218e05abae6eaa29c1f5c
oseoko <oseoko@naver.com>
2024-09-20 17:14:26 +0900

    Feat: User Class 선언

---
+++ src/user.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self, name, user_id, password, loan_count=0, loan_date=None, is_admin=False):
+        self.name = name
+        self.user_id = user_id
+        self.password = password
+        self.loan_count = loan_count
+        self.loan_date = loan_date
+        self.is_admin = is_admin
+
+    def __str__(self):
+        return f"{self.name}, {self.user_id}, {self.password}, {self.loan_count}, {self.loan_date}, {self.is_admin}"
