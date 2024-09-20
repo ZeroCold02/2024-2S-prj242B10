c6912db9a4ed929099f0cd73e23931e4ce35b13d
oseoko <oseoko@naver.com>
2024-09-20 16:58:40 +0900

    Feat: BookManager class 선언

--- src/book.py
+++ src/book.py
@@ -5,4 +5,9 @@ class Book:
         self.is_loaned = is_loaned
 
     def __str__(self):
-        return f"{self.title}, {self.book_id}, {self.is_loaned}"
+        return f"{self.title}, {self.book_id}, {self.is_loaned}
+
+class BookManager:
+    def __init__(self, file_path='data/booklist.txt'):
+        self.file_path = file_path
+        self.books = self.load_books()
