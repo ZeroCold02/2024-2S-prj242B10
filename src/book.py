eda5125a2e2b469d2b879cfda42eb1742fb51f08
oseoko <oseoko@naver.com>
2024-09-20 17:00:42 +0900

    Feat: 책 정보를 파일에서 불러오는 함수 추가

--- src/book.py
+++ src/book.py
@@ -1,3 +1,6 @@
+import csv
+import os
+
 class Book:
     def __init__(self, title, book_id, is_loaned=False):
         self.title = title
@@ -5,9 +8,19 @@ class Book:
         self.is_loaned = is_loaned
 
     def __str__(self):
-        return f"{self.title}, {self.book_id}, {self.is_loaned}
+        return f"{self.title}, {self.book_id}, {self.is_loaned}"
 
 class BookManager:
     def __init__(self, file_path='data/booklist.txt'):
         self.file_path = file_path
         self.books = self.load_books()
+
+    def load_books(self):
+        books = []
+        if os.path.exists(self.file_path):
+            with open(self.file_path, 'r', encoding='utf-8') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    title, book_id, is_loaned = row
+                    books.append(Book(title, book_id, is_loaned == 'True'))
+        return books
